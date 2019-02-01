37187f1eca6591c87f26c66b9eb7c3c45a410e47
Danyllo Fellip <hubitygames@gmail.com>
2019-02-01 10:38:58 -0200

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+let figure = $(".video").hover(hoverVideo, hideVideo);
+            function hoverVideo(e) {
+                $('video', this).get(0).play();
+            }
+            function hideVideo(e) {
+                $('video', this).get(0).pause();
+            }
